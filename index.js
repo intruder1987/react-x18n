dceaa18ab13ec06bc407dbb6456b31f744751b60
anatoliy <anatoliy.shirokikh@quantumsoft.ru>
2017-11-27 11:26:48 +0700

    Replaced React.createClass to React.Component

--- index.js
+++ index.js
@@ -1,52 +1,44 @@
 'use strict'
 
-let React = require('react')
-let X18N = require('x18n')
+let React = require('react');
+let X18N = require('x18n');
 
-let X18NSpanElement = React.createClass({
-  getDefaultProps: function () {
-    return {isPlural: false, _args: []}
-  },
+class X18NSpanElement extends React.Component {
+    componentWillMount() {
+        X18N.on(['lang:change', 'dict:change'], this.forceUpdate);
+    };
 
-  _update: function () {
-    this.forceUpdate()
-  },
+    componentWillUnmount() {
+        X18N.off(['lang:change', 'dict:change'], this.forceUpdate);
+    };
 
-  componentWillMount: function () {
-    X18N.on(['lang:change', 'dict:change'], this._update)
-  },
+    render() {
+        let result = 'error';
+        const { isPlural = false, _args = [] } = this.props;
+        if (this.props.isPlural === true) {
+            let args = Array.prototype.slice.call(this.props._args);
+            let key = args.shift();
 
-  componentWillUnmount: function () {
-    X18N.off(['lang:change', 'dict:change'], this._update)
-  },
+            result = X18N.t(key).plural.apply(null, args);
+        } else {
+            result = X18N.t.apply(null, this.props._args);
+        }
 
-  render: function () {
-    let result = 'error'
-
-    if (this.props.isPlural === true) {
-      let args = Array.prototype.slice.call(this.props._args)
-      let key = args.shift()
-
-      result = X18N.t(key).plural.apply(null, args)
-    } else {
-      result = X18N.t.apply(null, this.props._args)
+        return React.createElement('span', {}, result);
     }
-
-    return React.createElement('span', {}, result)
-  }
-})
+};
 
 let react_x18n_t = function () {
-  return React.createElement(X18NSpanElement, {isPlural: false, _args: arguments}, '')
-}
+    return React.createElement(X18NSpanElement, {isPlural: false, _args: arguments}, '');
+};
 
 react_x18n_t.plural = function () {
-  return React.createElement(X18NSpanElement, {isPlural: true, _args: arguments}, '')
-}
+    return React.createElement(X18NSpanElement, {isPlural: true, _args: arguments}, '');
+};
 
 let react_x18n = {
-  x18n: X18N,
-  t: react_x18n_t
-}
+    x18n: X18N,
+    t: react_x18n_t
+};
 
-module.exports = react_x18n
+module.exports = react_x18n;
